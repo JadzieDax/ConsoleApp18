7348454c20f761e90a7981ff69630e734d0e641e
Dax <timoha.qq@mail.ru>
2024-10-14 11:35:03 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = SqrtCalc(3, 2) + SqrtCalc(5, 3) + SqrtCalc(11, 7);
+            Console.WriteLine($"Результат: {result:F3}");
+            Console.ReadLine();
+        }
+
+        static double SqrtCalc(double a, double b)
+        {
+            return (Math.Sqrt(a) - Math.Sqrt(b)) / (Math.Sqrt(a) + Math.Sqrt(b));
+        }
+    }
+}
